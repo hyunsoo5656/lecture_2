68813e4276b203ae3f7e02c8f583bd1af6a07cf1
name <hyunsoo5656@gmail.com>
2020-07-15 17:50:01 +0900

    import

---
+++ for_data.py
@@ -0,0 +1,17 @@
+
+
+#import data_dic
+#print(data_dic.elemetary_school['제공기관명'])
+
+
+
+#data_dic.print_ele()
+
+#import arg_greet1 
+
+
+import factorial_recursion as fr
+
+#arg_greet1_greet()
+
+fr.factorial(10)
